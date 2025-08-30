dd3f20954f10d7cc28c093199a63dbc8a2913d78
Brown Lee <tglee951008@gmail.com>
2025-08-30 13:48:16 +0900

    library 충돌 문제 및 hilt compiler 중복 충돌문제로 resource excludes로 무시

--- build-logic/conventions/src/main/java/com/multi/module/convention/Kotlin.kt
+++ build-logic/conventions/src/main/java/com/multi/module/convention/Kotlin.kt
@@ -25,6 +25,7 @@ internal fun Project.configureKotlinAndroid(
         packaging {
             resources {
                 excludes += "/META-INF/{AL2.0,LGPL2.1}"
+                excludes += "META-INF/gradle/incremental.annotation.processors"
             }
         }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,4 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp.devtools) apply false
     alias(libs.plugins.hilt.plugin) apply false
-    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
